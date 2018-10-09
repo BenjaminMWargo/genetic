a90429e38576c7b88d1dee5e0603dda62b98cb9c
BenW <bmw95@pitt.edu>
2018-10-09 18:14:32 -0400

    Added schedual generation.

--- genetic.java
+++ genetic.java
@@ -1,7 +1,12 @@
 import java.util.List;
 import java.util.Collections;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class genetic{
+    
     public static class room{
         public String roomName;
         public int roomSize;
@@ -80,16 +85,79 @@ public class genetic{
     public static void printSchedual(schedual s){
         System.out.println("CRN|Course|Proffessor|Size|Needs media|Room Name|Room Size|Room media?|Period |Days |Time ");
         for (course c :s.courseList){
-            System.out.println(c.CRN + " | " +c.courseName + " | " +c.prof + " | " + c.courseSize + " | " +c.room.roomName + " | " + c.room.roomSize + " | " +c.room.roomMedia + " | " +c.timeFrame.period + " | " +c.timeFrame.days + " | " +c.timeFrame.time);
+            System.out.println(c.CRN + " | " +c.courseName + " | " +c.prof + " | " + c.courseSize + " | " + c.courseMedia + "     | " +c.room.roomName + " | " + c.room.roomSize + " | " +c.room.roomMedia + "     | " +c.timeFrame.period + " | " +c.timeFrame.days + " | " +c.timeFrame.time);
         }
         System.out.println("Fitness:" + s.fitness);
     }
+    public static List<String[]> readFile(String file){
+       // String file = "rooms.txt";
+        List<String[]> data = new ArrayList<String[]>();
+        try{  
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+       
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+           data.add(line.split(" "));
+         }
+        bufferedReader.close();
+       } catch(Exception e){System.out.print("read error");}
+       //for(String[] i:data){
+       //    for(String j:i){
+       //         System.out.print(j + " ");
+      //     }
+      //     System.out.println("");
+      // }
+       
+       return data;
+    }
+    public static schedual makeSchedual(){
+        String ROOMFILE = "rooms.txt";
+        String COURSEFILE = "courses.txt";
+        String PERIODFILE = "periods.txt";
+        List<String[]> courseMaster = readFile(COURSEFILE);
+        List<String[]> roomMaster = readFile(ROOMFILE);
+        List<String[]> periodMaster = readFile(PERIODFILE);
+        List<room> roomList = new ArrayList<room>();
+        List<timeFrame> periodList = new ArrayList<timeFrame>();
+        boolean x;
+        schedual s = new schedual();
+       
+        for(String[] i :courseMaster){
+              //Load all courses into s.courseList     
+            if (Integer.parseInt(i[4])==1){
+                x = true;
+            }else {x= false;}
+            s.courseList.add(new course(Integer.parseInt(i[0]),i[1],i[2],Integer.parseInt(i[3]),x));
+        }
+        for(String[] i :roomMaster){
+            //Load all rooms into roomlist
+            if (Integer.parseInt(i[2])==1){
+                x = true;
+            }else {x= false;}
+            roomList.add(new room(i[0],Integer.parseInt(i[1]),x));
+        }
+        for(String[] i :periodMaster){
+            //Load all rooms into roomlist
+            periodList.add(new timeFrame(i[1],Integer.parseInt(i[2]),Integer.parseInt(i[0])));
+        }
+        for(course c:s.courseList){
+            //Give each course a random room and period
+            Collections.shuffle(periodList);
+            Collections.shuffle(roomList);
+            c.room = roomList.get(0).deepCopy();
+            c.timeFrame = periodList.get(0).deepCopy();
+        }
+        return s;
+    }
     public static void main(String args[]){
-        
-        schedual test = new schedual();
-        test.courseList.add(new course(0,"Math","Teacher Guy",20,true));
-        test.courseList.get(0).timeFrame = new timeFrame("MFW", 9, 0);
-        test.courseList.get(0).room = new room("Room 3", 30, false);
-        printSchedual(test);
+     
+        int size = 10;
+        schedual population = makeSchedual();
+        printSchedual(population);
+       // test.courseList.add(new course(0,"Math","Teacher Guy",20,true));
+      //  test.courseList.get(0).timeFrame = new timeFrame("MFW", 9, 0);
+     //   test.courseList.get(0).room = new room("Room 3", 30, false);
+      //  printSchedual(test);
     }
 }
